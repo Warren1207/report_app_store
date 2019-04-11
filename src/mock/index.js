16f9af461fe24b63297ca8e19f95779b508ae2f1
Warren1207 <35440270+Warren1207@users.noreply.github.com>
2019-04-11 11:12:12 +0800

    报告列表

--- src/mock/index.js
+++ src/mock/index.js
@@ -57,6 +57,90 @@ Mock.mock(RegExp("/ReportTemplate/GetReportTemplateList.*"), "get", function() {
   };
 });
 
+Mock.mock(RegExp("/TaskReport/GetTaskReportList.*"), "get", function() {
+  return {
+    State: 0,
+    FlagDescription: "SUCCESS",
+    CachedTime: "2019-04-10 11:24:49.806400",
+    Data: [
+      {
+        Id: 1308,
+        Name: "无人值守PingCall0126-无人ZA",
+        State: 1,
+        Type: 1,
+        TemplateId: 0,
+        RateProgress: 100,
+        DownloadCount: 0,
+        DownloadUrl:
+          "D:\\skyrulerIncrement\\2019\\1\\无人值守PingCall0126-无人ZA\\无人值守PingCall0126-无人ZA.zip",
+        CreateTime: "2019-01-26 15:30:00",
+        Remarks: null,
+        ServerId: 32,
+        TaskId: 21480,
+        Parameter: null,
+        Consignee: null,
+        IsSendAppendix: 0,
+        CreateUserId: 46,
+        CreateUserName: "Test1",
+        ErrorMessage: "",
+        Guid: "b8cfdfcd-a0b8-4245-aadc-a94d38ae7f8e",
+        IsDownload: true,
+        UpdateTime: "2019-01-26 15:30:20"
+      },
+      {
+        Id: 1307,
+        Name: "无人值守PingCall0126-无人值守PingCAllZA-无人值守PingCA_3",
+        State: 1,
+        Type: 1,
+        TemplateId: 0,
+        RateProgress: 100,
+        DownloadCount: 0,
+        DownloadUrl:
+          "D:\\skyrulerIncrement\\2019\\1\\无人值守PingCall0126-无人值守PingCAllZA-无人值守PingCA_3\\无人值守PingCall0126-无人值守PingCAllZA-无人值守PingCA_3.zip",
+        CreateTime: "2019-01-26 15:30:00",
+        Remarks: null,
+        ServerId: 32,
+        TaskId: 21466,
+        Parameter: null,
+        Consignee: null,
+        IsSendAppendix: 0,
+        CreateUserId: 46,
+        CreateUserName: "Test1",
+        ErrorMessage: "",
+        Guid: "dbb158a3-62e8-4a3f-ac9c-4a52a799b26f",
+        IsDownload: true,
+        UpdateTime: "2019-01-26 15:30:20"
+      },
+      {
+        Id: 1306,
+        Name: "无人值守PingCall0126-无人值守PingCall_9",
+        State: 1,
+        Type: 1,
+        TemplateId: 0,
+        RateProgress: 100,
+        DownloadCount: 0,
+        DownloadUrl:
+          "D:\\skyrulerIncrement\\2019\\1\\无人值守PingCall0126-无人值守PingCall_9\\无人值守PingCall0126-无人值守PingCall_9.zip",
+        CreateTime: "2019-01-26 15:30:00",
+        Remarks: null,
+        ServerId: 32,
+        TaskId: 21462,
+        Parameter: null,
+        Consignee: null,
+        IsSendAppendix: 0,
+        CreateUserId: 46,
+        CreateUserName: "Test1",
+        ErrorMessage: "",
+        Guid: "22b3a0c7-d483-41cb-9170-9fa32c524189",
+        IsDownload: true,
+        UpdateTime: "2019-01-26 15:30:20"
+      }
+    ],
+    TotalCount: 3,
+    Message: "OK"
+  };
+});
+
 Mock.mock("/BaseStation/AddBaseStation", "post", function() {
   return {
     State: 0,
--- src/views/ReportList.vue
+++ src/views/ReportList.vue
@@ -1,10 +1,106 @@
 <template>
-  <div>reportlist</div>
+  <div class="wrap">
+    <el-row>
+      <el-col :span="4"
+        ><el-input v-model="queryParams.name" placeholder="报告名称"></el-input
+      ></el-col>
+      <el-col :span="4">
+        <el-select v-model="queryParams.status" clearable placeholder="状态">
+          <el-option label="成功" :value="1"> </el-option>
+          <el-option label="失败" :value="2"> </el-option>
+          <el-option label="进行中" :value="0"> </el-option> </el-select
+      ></el-col>
+      <el-col :span="4" style="text-align: left;">
+        <el-button type="primary" @click="queryFn">搜索</el-button>
+      </el-col>
+    </el-row>
+    <div class="table-wrap">
+      <el-table
+        ref="reportTable"
+        :data="queryData"
+        tooltip-effect="dark"
+        style="width: 100%"
+        height="100%"
+      >
+        <el-table-column type="selection" width="55"> </el-table-column>
+        <el-table-column type="index" width="55"> </el-table-column>
+        <el-table-column prop="Name" label="报告名称" show-overflow-tooltip>
+        </el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <span>{{ statusObj[scope.row.State] }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="类型">
+          <template slot-scope="scope">
+            <span>{{ scope.row.Type === 0 ? "手动" : "自动" }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="进度条">
+          <template slot-scope="scope">
+            <span>{{ scope.row.RateProgress + "%" }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="DownloadCount" label="下载次数">
+        </el-table-column>
+        <el-table-column prop="CreateTime" label="创建时间"> </el-table-column>
+        <el-table-column prop="UpdateTime" label="更新时间"> </el-table-column>
+        <el-table-column prop="Remarks" label="备注" show-overflow-tooltip>
+        </el-table-column>
+      </el-table>
+    </div>
+    <el-pagination
+      @size-change="pageSizeFn"
+      @current-change="pageChangeFn"
+      :current-page="queryParams.pageIndex"
+      :page-sizes="[10, 20, 30, 100]"
+      :page-size="queryParams.pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="pageCount"
+    >
+    </el-pagination>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "reportlist"
+  name: "reportlist",
+  data() {
+    return {
+      queryParams: {
+        pageIndex: 1,
+        pageSize: 10
+      },
+      queryData: [],
+      pageCount: 0,
+      statusObj: {
+        1: "成功",
+        2: "失败",
+        0: "进行中"
+      }
+    };
+  },
+  methods: {
+    queryFn() {
+      this.$fetch("/TaskReport/GetTaskReportList", this.queryParams).then(
+        res => {
+          this.queryData = res.Data;
+          this.pageCount = res.TotalCount;
+        }
+      );
+    },
+    pageSizeFn(val) {
+      this.queryParams.pageSize = val;
+      this.queryFn();
+    },
+    pageChangeFn(val) {
+      this.queryParams.pageIndex = val;
+      this.queryFn();
+    }
+  },
+  created() {
+    this.queryFn();
+  }
 };
 </script>
 
