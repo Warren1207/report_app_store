98b80a687c0ee4444c19aa64205a533c8d964a23
Warren1207 <35440270+Warren1207@users.noreply.github.com>
2019-04-16 08:43:57 +0800

    菜单修改

--- src/router.js
+++ src/router.js
@@ -24,7 +24,7 @@ const router = new Router({
       path: "/",
       name: "Layout",
       component: Layout,
-      redirect: "uploadStation",
+      redirect: "reportList",
       children: [
         {
           path: "uploadStation",
