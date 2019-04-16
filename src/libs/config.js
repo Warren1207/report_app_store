8af1f515eca4ba9da958ff266ceddc36c277281c
Warren1207 <35440270+Warren1207@users.noreply.github.com>
2019-04-16 11:37:40 +0800

    下载模板路径获取改为接口

--- src/libs/config.js
+++ src/libs/config.js
@@ -1,5 +1,3 @@
 export default {
-  baseUrl: "http://10.168.1.120:8030/",
-  stationTemplateUrl:
-    "E:\\Source\\ReportingAppStore\\ReportingAppStore.Web\\UploadFiles\\BaseStationTemplate\\工参模板.xlsx"
+  baseUrl: "http://10.168.1.120:8030/"
 };
